3ffaab6392ac1cc56084859ebeffe3b26f62fa1c
sgangele21 <sgangele@gmu.edu>
2021-11-14 00:19:00 -0500

    Add barebones UI for search

--- AppStoreReviewsSearcher/AppStoreReviewsSearcher/ContentView.swift
+++ AppStoreReviewsSearcher/AppStoreReviewsSearcher/ContentView.swift
@@ -9,22 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var reviews: [Review]
+    @State private var reviews: [Review] = []
+    @State private var query: String = ""
     let reviewsFetcher = ReviewsFetcher()
     
     var body: some View {
-        Text("Hello, world!")
-            .onAppear {
-                Task {
-                    do {
-                        reviews = try await reviewsFetcher.fetchAllReviews()
-                        print(reviews.count) // Prints 500
-                    } catch(let error) {
-                        print("❌ Error fetching Reviews: \(error)")
-                    }
-                    
+        VStack {
+            HStack {
+                TextField("Search App Store Reviews", text: $query, prompt: Text("Enter query here"))
+                Button("Search") {
+                    print("Searching for: \(query)")
                 }
+            }.padding()
+            Spacer()
+        }.padding()
+        .onAppear {
+            Task {
+                do {
+                    reviews = try await reviewsFetcher.fetchAllReviews()
+                    print(reviews.count) // Prints 500
+                } catch(let error) {
+                    print("❌ Error fetching Reviews: \(error)")
+                }
+                
             }
+        }
     }
 }
 
@@ -33,6 +42,6 @@ struct ContentView_Previews: PreviewProvider {
     
     
     static var previews: some View {
-        ContentView(reviews: [])
+        ContentView()
     }
 }
